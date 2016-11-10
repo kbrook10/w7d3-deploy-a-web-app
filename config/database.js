4a3bae3a469e973698062810e8a416f44476577a
keith brooks <kbrook10@gmail.com>
2016-11-10 10:58:53 -0500

    Update posts migration

--- config/database.js
+++ config/database.js
@@ -65,14 +65,18 @@ module.exports = {
   | npm i --save pg
   |
   */
+  // pg: {
+  //   client: 'pg',
+  //   connection: {
+  //     host: Env.get('DB_HOST', 'localhost'),
+  //     user: Env.get('DB_USER', 'root'),
+  //     password: Env.get('DB_PASSWORD', ''),
+  //     database: Env.get('DB_DATABASE', 'adonis')
+  //   }
+  // }
   pg: {
     client: 'pg',
-    connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    }
+    connection: Env.get('DATABASE_URL') //Use this to make DB work with Heroku
   }
 
 }
